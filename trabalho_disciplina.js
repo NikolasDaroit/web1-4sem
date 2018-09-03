5b254a7012c8f5e09e4b08671d06b1d08da07761
Nikolas Daroit <nikolastnd@gmail.com>
2018-09-03 11:23:53 -0300

    trabalho da disciplina

--- trabalho_disciplina.js
+++ trabalho_disciplina.js
@@ -1,24 +1,39 @@
 
 function appendImage(number, klass){
-	console.log(number, klass)
-	card  =  '<div class="card" data-image="'+number+'">'
+	// console.log(number, klass)
+	card  =   '<div class="card" data-image="'+number+'">'
 			+ '  <img src="https://picsum.photos/200/200/?image='+number+'" alt="" style="width:100%">'
 			+ '  <div class="container">'
 			+ '    <p>Architect & Engineer</p> '
 			+ '  </div>'
 			+ '</div>'
 
+
 	$( card ).appendTo( klass );
 }
+function appendToTable(number, klass){
+	// console.log(number, klass)
+	card  =  '<div class="card" data-image="'+number+'">'
+			+ '  <img src="https://picsum.photos/100/100/?image='+number+'" alt="" style="width:100%">'
+			+ '</div>'
+
+	item =   '<tr><td>'+card+'</td>'
+			+'<td>'+(number%7)+'</td>'
+			+'<td>'+number+'</td>'
+			+'<td class="price">'+(number*(number%7))+'</td><tr>'
+
+	// $( item ).appendTo( klass );
+	return item;
+}
+
 $(function() {
 
 	$('#cart').on('click', function(){
 		console.log('click');
 		window.location.href = './trabalho_disciplina_carrinho.html'
 	})
-
-	$("#cart").click(function() {
+	$('#products').on('click', function(){
 		console.log('click');
-	  window.location.href = './trabalho_disciplina_carrinho.html'
-	});
+		window.location.href = './trabalho_disciplina.html'
+	})
 });
